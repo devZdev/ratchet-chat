ad60003f23c204b64e9b27651135410d8978ce5a
Devin Zimmer <dz@renttherunway.com>
2019-08-21 16:17:55 +0100

    react server

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,4 +1,24 @@
 <?php
+$app = new \Ratchet\Http\HttpServer(
+    new \Ratchet\WebSocket\WsServer(
+        new \MyApp\Chat()
+    )
+);
+
+$loop = \React\EventLoop\Factory::create();
+
+$secure_websockets = new \React\Socket\Server('0.0.0.0:8083', $loop);
+$secure_websockets = new \React\Socket\SecureServer($secure_websockets, $loop, [
+    'local_cert' => 'path-to/public.pem',
+    'local_pk' => 'path-to/private.pem',
+    'verify_peer' => false
+]);
+
+$secure_websockets_server = new \Ratchet\Server\IoServer($app, $secure_websockets, $loop);
+$secure_websockets_server->run();
+
+
+/*
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
@@ -16,3 +36,4 @@ use MyApp\Chat;
     );
 
     $server->run();
+    */
